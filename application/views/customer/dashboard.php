114a2f4f7f78920b2d62b4cdff659d8fcc6e279e
muhamad iqbal <fajri7037@gmail.com>
2022-06-21 22:10:31 +0700

    Initial commit

---
+++ application/views/customer/dashboard.php
@@ -0,0 +1,174 @@
+<!-- Page Content -->
+<!-- Banner Starts Here -->
+<div class="main-banner header-text">
+  <div class="container-fluid">
+    <div class="owl-banner owl-carousel">
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-1-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+
+            </div>
+            <a href="fleet.html">
+            </a>
+            <ul class="post-info">
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-2-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+
+            </div>
+            <a href="fleet.html">
+            </a>
+            <ul class="post-info">
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-3-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+
+            </div>
+            <a href="fleet.html">
+            </a>
+            <ul class="post-info">
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-4-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+
+            </div>
+            <a href="fleet.html">
+            </a>
+            <ul class="post-info">
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-5-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+
+            </div>
+            <a href="fleet.html">
+            </a>
+            <ul class="post-info">
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-6-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+            </div>
+            <a href="fleet.html">
+            </a>
+            <ul class="post-info">
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Banner Ends Here -->
+
+<section class="blog-posts grid-system">
+  <div class="container">
+    <div class="all-blog-posts">
+      <h2 class="text-center">Jenis Mobil</h2>
+      <br>
+      <div class="row">
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/toyota-camry.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <strong>Rental Dari Harga</strong> <span>Rp. 400.000</span> <strong>Per hari</strong>
+              <a href="offers.html">
+                <h4>toyota-camry</h4>
+              </a>
+              <div class="post-options">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <ul class="post-tags">
+                      <li><i class="fa fa-bullseye"></i></li>
+                      <li><a href="<?= base_url('customer/data_mobil') ?>">Detail Mobil</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/avanza-hitam.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <strong>Rental Dari Harga</strong> <span>Rp. 350.000</span> <strong>Per hari</strong>
+              <a href="offers.html">
+                <h4>avanza-hitam</h4>
+              </a>
+              <div class="post-options">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <ul class="post-tags">
+                      <li><i class="fa fa-bullseye"></i></li>
+                      <li><a href="<?= base_url('customer/data_mobil') ?>">Detail Mobil</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/avanza-putih.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <strong>Rental Dari Harga</strong> <span>Rp. 350.000</span> <strong>Per hari</strong>
+              <a href="offers.html">
+                <h4>avanza-putih</h4>
+              </a>
+              <div class="post-options">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <ul class="post-tags">
+                      <li><i class="fa fa-bullseye"></i></li>
+                      <li><a href="<?= base_url('customer/data_mobil') ?>">Detail Mobil</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<div style="height: 180px;"></div>
